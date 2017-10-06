820502e645d8ac2b3ee7c1a8139c8edcb3a85f9b
Esprit Orgue <amir.alwash@gmail.com>
2017-10-06 13:54:14 +0200

    swift

--- ex-03/part2/Sources/part2/main.swift
+++ ex-03/part2/Sources/part2/main.swift
@@ -3,6 +3,9 @@ print("Hello, world!")
 
 let p0 = PTPlace(named: "p0")
 let p1 = PTPlace(named: "p1")
+let r0 = PTPlace(named: "r0")
+let q1 = PTPlace(named: "q1")
+let q0 = PTPlace(named: "q0")
 
 let t0 = PTTransition(
     named         : "t0",
@@ -12,5 +15,13 @@ let t1 = PTTransition(
     named         : "t1",
     preconditions : [PTArc(place: p1)],
     postconditions: [PTArc(place: p0)])
+let t2 = PTTransition(
+        named         : "t1",
+        preconditions : [PTArc(place: p1)],
+        postconditions: [PTArc(place: p0)])
+let t3 = PTTransition(
+            named         : "t1",
+            preconditions : [PTArc(place: p1)],
+            postconditions: [PTArc(place: p0)])
 
 let pn = PTNet(places: [p0, p1], transitions: [t0, t1])
