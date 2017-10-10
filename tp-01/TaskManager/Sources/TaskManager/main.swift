774cae1ca0e640fd918311b7fba789be493f6d05
Esprit Orgue <amir.alwash@gmail.com>
2017-10-10 17:30:39 +0200

    Tp01 Try

--- tp-01/TaskManager/Sources/TaskManager/main.swift
+++ tp-01/TaskManager/Sources/TaskManager/main.swift
@@ -1,5 +1,7 @@
 import TaskManagerLib
 
+print("Exercice 3: Problème du réseau de pétri")
+
 let taskManager = createTaskManager()
 
 // Show here an example of sequence that leads to the described problem.
@@ -7,7 +9,30 @@ let taskManager = createTaskManager()
 //     let m1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0])
 //     let m2 = spawn.fire(from: m1!)
 //     ...
+let create = taskManager.transitions.filter{$0.name == "create"}[0]
+let spawn = taskManager.transitions.filter{$0.name == "spawn"}[0]
+let exec = taskManager.transitions.filter{$0.name == "exec"}[0]
+let success = taskManager.transitions.filter{$0.name == "success"}[0]
+let fail = taskManager.transitions.filter{$0.name == "fail"}[0]
 
+let taskPool = taskManager.places.filter{$0.name == "taskPool"}[0]
+let processPool = taskManager.places.filter{$0.name == "processPool"}[0]
+let inProgress = taskManager.places.filter{$0.name == "inProgress"}[0]
+
+let m1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0])
+print("m1 (create) ",m1)
+let m2 = spawn.fire(from: m1!)
+print("m2 (spawn) ",m2)
+let m3 = spawn.fire(from: m2!)
+print("m3 (spawn)",m3)
+let m4 = exec.fire(from: m3!)
+print("m4 (exec) ",m4)
+let m5 = success.fire(from: m4!)
+print("m5 (success)",m5)
+let m6 = exec.fire(from: m5!)
+print("m6 (exec) ",m6)
+
+print("Correct TaskManager")
 let correctTaskManager = createCorrectTaskManager()
 
 // Show here that you corrected the problem.
@@ -15,3 +40,26 @@ let correctTaskManager = createCorrectTaskManager()
 //     let m1 = create.fire(from: [taskPool: 0, processPool: 0, inProgress: 0])
 //     let m2 = spawn.fire(from: m1!)
 //     ...
+let create2 = correctTaskManager.transitions.filter{$0.name == "create"}[0]
+let spawn2 = correctTaskManager.transitions.filter{$0.name == "spawn"}[0]
+let exec2 = correctTaskManager.transitions.filter{$0.name == "exec"}[0]
+let success2 = correctTaskManager.transitions.filter{$0.name == "success"}[0]
+let fail2 = correctTaskManager.transitions.filter{$0.name == "fail"}[0]
+
+let taskPool2 = correctTaskManager.places.filter{$0.name == "taskPool"}[0]
+let processPool2 = correctTaskManager.places.filter{$0.name == "processPool"}[0]
+let inProgress2 = correctTaskManager.places.filter{$0.name == "inProgress"}[0]
+let complement = correctTaskManager.places.filter{$0.name == "complement"}[0]
+
+let m21 = create2.fire(from: [taskPool2: 0, processPool2: 0, inProgress2: 0, complement: 0])
+print("m1 (create) ",m21)
+let m22 = spawn2.fire(from: m21!)
+print("m2 (spawn) ",m22)
+let m23 = spawn2.fire(from: m22!)
+print("m3 (spawn)",m23)
+let m24 = exec2.fire(from: m23!)
+print("m4 (exec) ",m24)
+let m25 = success2.fire(from: m24!)
+print("m5 (success)",m25)
+let m26 = exec2.fire(from: m25!)
+print("m6 (exec) ",m26)
